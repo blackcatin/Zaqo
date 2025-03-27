1110f5845c33e84e9a2cc6267d6e844587251dad
cardinaldeacre <cardinaldeacre@gmail.com>
2025-03-27 11:31:53 +0700

    Iqbal => navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,22 +1,30 @@
 import './styles/navbar.css';
+import infaqImage from './assets/infaq.jpg';
 
 function Navbar() {
 	return (
 		<nav>
 			<div className="container-navbar">
-				<img className="navbar-icon" src="assets" alt="" />
-				<h1>LOGO</h1>
-				<ul>
-					<li>
-						<a href="#">Home</a>
-					</li>
-					<li>
-						<a href="#">About</a>
-					</li>
-					<li>
-						<a href="#">Contact</a>
-					</li>
-				</ul>
+				<a href="index.html">
+					<h2>ZAQO.</h2>
+				</a>
+				<a href="">
+					<img src={infaqImage} alt="cek" />
+				</a>
+				<a class="a" href="index.html">
+					Beranda
+				</a>
+				<a class="b" href="calculator.html">
+					Kalkulator
+				</a>
+				<a class="c" href="modul.html">
+					Pembelajaran
+				</a>
+				<div class="navbar-menu">
+					<a href="#" id="menu-icon">
+						<img src="images/menu-w.png" alt="menu-icon" />
+					</a>
+				</div>
 			</div>
 		</nav>
 	);
